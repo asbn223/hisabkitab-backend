ee4586d60c47298f921c2807b4cfa1b911f6b14b
asbn223 <asbn2231@gmail.com>
2026-05-05 12:56:40 +0545

    Remove Logging

--- build.sh
+++ build.sh
@@ -4,8 +4,5 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# Create logs directory for RotatingFileHandler
-mkdir -p logs
-
 python manage.py collectstatic --no-input
 python manage.py migrate
